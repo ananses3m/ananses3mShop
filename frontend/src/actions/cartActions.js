4a1478ded2a6aafe793f7c95cc9c661900635ac5
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2021-09-02 00:19:30 -0600

    User authentication and authorization implemented in backend. Password being hashed

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,29 +1,30 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 import axios from 'axios';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-    // try {
-        // dispatch({ type: CART_ADD_ITEM })
 
-        const { data } = await axios.get(`/api/products/${id}`)
+    const { data } = await axios.get(`/api/products/${id}`)
 
-        dispatch({
-            type: CART_ADD_ITEM,
-            payload: {
-                product: data._id,
-                name: data.name,
-                image: data.image,
-                price: data.price,
-                countInStock: data.countInStock,
-                qty
-            }
-        })
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload: {
+            product: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            qty
+        }
+    })
 
-        localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-    // } catch (error) {
-        // dispatch({
-            // type: CART_ADD_ITEM,
-            // payload: error.response && error.response.data.message ? error.response.data.message : error.message
-        // })
-    // }
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
