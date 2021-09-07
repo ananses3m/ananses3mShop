9743c6910beb747fede74b37295d5512e2d94c58
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2021-09-07 01:04:53 -0600

    Preparing application for deployment

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,10 @@ app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+}
+
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
