8aafcf949912d0e4a7321dde6cd123fa199ce858
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-24 23:56:48 -0700

    Code cleanup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+import momo from 'mtn-momo';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -22,7 +23,6 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
@@ -33,8 +33,38 @@ app.use('/api/upload', multerRoutes);
 // app.use('/api/upload', cloudinaryRoutes);
 // app.use('/api/upload', uploadRoutes);
 
+const { Collections } = momo.create({
+    callbackHost: process.env.CALLBACK_HOST
+});
+
+const collections = Collections({
+    userSecret: process.env.MTN_MOMO_COLLECTIONS_USER_SECRET,
+    userId: process.env.MTN_MOMO_COLLECTIONS_USER_ID,
+    primaryKey: process.env.MTN_MOMO_COLLECTIONS_PRIMARY_KEY
+});
+
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
-// app.get('/api/config/momo', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+
+app.post('/api/config/momo', async (req, res) => {
+    try {
+        const transactionId = await collections.requestToPay({
+            amount: req.body.reqAmount,
+            currency: "EUR",
+            externalId: "123456",
+            payer: {
+                partyIdType: "MSISDN",
+                partyId: req.body.payerNumber
+            },
+            payerMessage: "testing",
+            payeeNote: (req.body.order_id).toString()
+        })
+        res.json(collections.getTransaction(transactionId))
+        console.log('Momo: ', collections.getBalance())
+    } catch (error) {
+        console.error(error)
+    }
+    
+});
 
 const __dirname = path.resolve();
 // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
